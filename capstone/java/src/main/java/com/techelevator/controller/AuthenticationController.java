072e7b3353c2b626a486be94fd9e544b47c85c6e
Alex Lewonas <alewonas@gmail.com>
2022-12-06 17:18:40 -0500

    add to dj table

--- capstone/java/src/main/java/com/techelevator/controller/AuthenticationController.java
+++ capstone/java/src/main/java/com/techelevator/controller/AuthenticationController.java
@@ -57,6 +57,12 @@ public class AuthenticationController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User Already Exists.");
         } catch (UsernameNotFoundException e) {
             userDao.create(newUser.getUsername(),newUser.getPassword(), newUser.getRole());
+
+            if(newUser.getRole().equals("DJ")){
+                User user = userDao.findByUsername((newUser.getUsername()));
+                userDao.createDj(user.getId());
+            }
+
         }
     }
 
