491eb4242ebe6970af7600304726282df983dbce
Allen Kramer <arkramer702@gmail.com>
2022-12-07 16:17:12 -0500

    Create and Delete genre functionality with new GenreController

---
+++ capstone/java/src/main/java/com/techelevator/controller/GenreController.java
@@ -0,0 +1,38 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.GenreDao;
+import com.techelevator.dao.UserDao;
+import com.techelevator.model.Genre;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@PreAuthorize("isAuthenticated()")
+@RequestMapping("/genre")
+public class GenreController {
+
+    private GenreDao genreDao;
+
+    public GenreController(GenreDao genreDao){
+        this.genreDao = genreDao;
+    }
+
+    @PreAuthorize("hasRole('DJ')")
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/create", method = RequestMethod.POST)
+    public void createGenre(@RequestBody Genre genre){
+        genreDao.createGenre(genre);
+    }
+
+    @PreAuthorize("hasRole('DJ')")
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/delete", method = RequestMethod.DELETE)
+    public void deleteGenre(@RequestBody Genre genre){
+        genreDao.deleteGenre(genre);
+    }
+
+
+
+
+}
