0fc8e6e97ebd0006fe6e4cb499deedb1a801794a
Jaevon Stokes <jayrock89@msn.com>
2022-12-06 21:46:56 -0500

    jae2

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <img alt="Elevate logo" src="./images/Elevate.png" />'
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
@@ -7,3 +8,23 @@
     <router-view />
   </div>
 </template>
+
+
+<style scoped>
+#app {
+   background-color: #000000;
+   height: 100vh;
+}
+#nav{
+  background-color: #4A007B;
+  font-family: "Audiowide", sans-serif;
+   
+}
+a{
+  color: #090531;
+}
+
+main {
+  background-image: url(./Images);
+}
+</style>
