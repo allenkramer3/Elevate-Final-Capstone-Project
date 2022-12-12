a7221ccb2b7028066b889b2a03eb6acab8f7a5a8
Alex Lewonas <alewonas@gmail.com>
2022-12-12 10:25:05 -0500

    scopes added

--- capstone/vue/src/components/AuthorizePage.vue
+++ capstone/vue/src/components/AuthorizePage.vue
@@ -2,7 +2,7 @@
   <div>
       <h1>Test 123; add auth stuff in this component</h1>
       <h2>Click to get auth code</h2>
-      <a v-on:click="getParams()" href="https://accounts.spotify.com/authorize?client_id=39164483b245448a9b0816a999571e93&redirect_uri=http://localhost:8080/dj/authorize&response_type=code&scope=user-modify-playback-state+user-read-playback-position+user-library-read+streaming+user-read-playback-state">Authorize</a>
+      <a v-on:click="getParams()" href="https://accounts.spotify.com/authorize?client_id=39164483b245448a9b0816a999571e93&redirect_uri=http://localhost:8080/dj/authorize&response_type=code&scope=user-read-playback-state+user-modify-playback-state+user-read-currently-playing app-remote-control+streaming playlist-read-private+playlist-modify-public+user-follow-modify+user-follow-read+user-read-playback-position+user-top-read+user-read-recently-played+user-library-modify+user-library-read">Authorize</a>
         <br>
         <h3>Code: </h3>
         <h3 id="auth-code"> </h3>
